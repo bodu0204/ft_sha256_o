5ef1aa1d0a9f51a78a75b4c19b173e2497d8930f
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-05-04 20:59:36 +0900

    fixed-2

--- tool.c
+++ tool.c
@@ -8,16 +8,16 @@ uint32_t	rbit(uint32_t x, uint32_t n)
 
 void	b32tob8(uint32_t *src, uint8_t *dst)
 {
-	dest[0] = (*src >> 24);
-	dest[1] = (*src >> 16) & 0xff;
-	dest[2] = (*src >> 8) & 0xff;
-	dest[3] = *src & 0xff;
+	dst[0] = (*src >> 24);
+	dst[1] = (*src >> 16) & 0xff;
+	dst[2] = (*src >> 8) & 0xff;
+	dst[3] = *src & 0xff;
 	return ;
 }
 
 void	b8tob32(uint8_t *src, uint32_t *dst)
 {
-	*dest = (((uint32_t)src[0]) << 24) | (((uint32_t)src[1]) << 16) \
+	*dst = (((uint32_t)src[0]) << 24) | (((uint32_t)src[1]) << 16) \
 	 | (((uint32_t)src[2]) << 8) | ((uint32_t)src[3]);
 	return ;
 }
