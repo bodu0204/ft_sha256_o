72e9e507b457d0628d9398e9b27880186e020cbe
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-05-05 07:51:35 +0900

    fixed

--- sha256_done.c
+++ sha256_done.c
@@ -1,4 +1,5 @@
 #include"sha256.h"
+#include"debug.h"
 
 void	sha256_done(t_sha *ctx, uint8_t *hdst)
 {
@@ -14,6 +15,7 @@ void	sha256_done(t_sha *ctx, uint8_t *hdst)
 	}
 	if (ctx->len > 55)
 	{
+TEST
 		hash(ctx);
 		i = 0;
 		while (i < 64)
@@ -30,8 +32,9 @@ void	sha256_done(t_sha *ctx, uint8_t *hdst)
 	//ctx->buf[58] = _shb(ctx->bits[1],  8);
 	//ctx->buf[57] = _shb(ctx->bits[1], 16);
 	//ctx->buf[56] = _shb(ctx->bits[1], 24);
-	b8tob32(ctx->buf + 60, ctx->bits);
-	b8tob32(ctx->buf + 56, ctx->bits + 1);
+	b32tob8(ctx->bits + 1, ctx->buf + 60);
+	b32tob8(ctx->bits, ctx->buf + 56);
+TEST
 	hash(ctx);
 	//if (hash != NULL) {
 	//	for (i = 0, j = 24; i < 4; i++, j -= 8) {
--- sha256_hash.c
+++ sha256_hash.c
@@ -16,6 +16,7 @@ void	sha256_hash(t_sha *ctx, const void *data, size_t len)
 		ctx->len++;
 		i++;
 		if (ctx->len == sizeof(ctx->buf)) {
+TEST
 			hash(ctx);
 			ctx->len = 0;
 		}
@@ -48,7 +49,7 @@ void	hash(t_sha *ctx)
 			ctx->W[16] = G1(ctx->W[14]) + ctx->W[9] + G0(ctx->W[1]) + ctx->W[0];
 		}
 /* */char c;
-TESTsx("a", hs[0])
+/* TESTsx("a", hs[0])
 TESTsx("b", hs[1])
 TESTsx("c", hs[2])
 TESTsx("d", hs[3])
@@ -56,6 +57,24 @@ TESTsx("e", hs[4])
 TESTsx("f", hs[5])
 TESTsx("g", hs[6])
 TESTsx("h", hs[7])
+ */
+TESTnx(i, ctx->W[0])
+TESTnx(i + 1, ctx->W[1])
+TESTnx(i + 2, ctx->W[2])
+TESTnx(i + 3, ctx->W[3])
+TESTnx(i + 4, ctx->W[4])
+TESTnx(i + 5, ctx->W[5])
+TESTnx(i + 6, ctx->W[6])
+TESTnx(i + 7, ctx->W[7])
+TESTnx(i + 8, ctx->W[8])
+TESTnx(i + 9, ctx->W[9])
+TESTnx(i + 10, ctx->W[10])
+TESTnx(i + 11, ctx->W[11])
+TESTnx(i + 12, ctx->W[12])
+TESTnx(i + 13, ctx->W[13])
+TESTnx(i + 14, ctx->W[14])
+TESTnx(i + 15, ctx->W[15])
+TESTnx(i + 16, ctx->W[16])
 read(0, &c, 1);
 		t[0] = hs[7] + S1(hs[4]) + Ch(hs[4], hs[5], hs[6]) + ctx->K[i] + ctx->W[16];
 		t[1] = S0(hs[0]) + Ma(hs[0], hs[1], hs[2]);
